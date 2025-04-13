af16bd25d701d86a53cae5029468d9847996b924
Todd Martin <kj4ntv@gmail.com>
2025-04-12 20:25:01 -0700

    fix: get working on macOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
-#include <iostream>
+#define GL_SILENCE_DEPRECATION
+#include <GLFW/glfw3.h>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  if(!glfwInit()) {
+    return -1;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  GLFWwindow* window = glfwCreateWindow(1280, 720, "Hello, World", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  while(!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
